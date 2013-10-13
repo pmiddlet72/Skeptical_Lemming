21dbe2a49c80cc9e61d6c89454ead69916bf4a3b
Phillip Middleton <pmiddlet72@gmail.com>
2013-10-13 12:57:04 -0500

    Initial project settings for SkeptLem Django

---
+++ local_settings.py
@@ -0,0 +1,21 @@
+
+DEBUG = True
+
+TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner'
+
+DATABASES = {
+    "default": {
+        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "pup",
+        # Not used with sqlite3.
+        "USER": "phil4485",
+        # Not used with sqlite3.
+        "PASSWORD": "dp|$ea1",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "localhost",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "",
+    }
+}
